c9ac82575ca194432648900cfe34e52a1068c883
Le Huynh Phuc <leehuynhphuc@gmail.com>
2022-05-12 23:27:13 +0700

    them khoang cach

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main(){
     cout << "hello github";
     cout << "thu xem saoo";
     for (int i = 0; i < 5; i++){
-        cout << i << "\n";
+        cout << "\n" << i << "\n";
     }
     
     return 0;
